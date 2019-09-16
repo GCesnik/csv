75c1974d1f2b38e719052af2eebea86bb8e704c1
Greg Cesnik <greg.cesnik@metova.com>
2019-09-16 11:37:02 -0500

    Heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,19 @@
-require 'sidekiq'
+if Rails.env.development?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 20 }
+  end
 end
 
-Sidekiq.configure_server do |config|
-  config.redis = { :size => 2 }
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 20 }
+  end
 end
