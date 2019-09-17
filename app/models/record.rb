10be143e21979561c9cfd30499c6c10a63c84f0a
Greg Cesnik <greg.cesnik@metova.com>
2019-09-17 14:53:43 -0500

    Fix error messages

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,14 +4,14 @@ class Record < ApplicationRecord
   validates :phone, phone: { possible: true, message: 'number must be valid' }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be valid' }
   validates :first, :last, length: { minimum: 2, allow_blank: true, message: 'name should be at least 2 characters long' }
-  validates_format_of :first, :last, { with: /\A[a-zA-Z]*\z/, message: 'can only be letters' }
+  validates_format_of :first, :last, { with: /\A[a-zA-Z]*\z/, message: 'name can only be letters' }
   validate :last_without_first
 
   before_create :strip_phone_punctuation
 
   def last_without_first
     if self.first.nil? && !self.last.nil?
-      errors.add(:last, "can't be present without a first name")
+      errors.add(:last, "name can't be present without a first name")
     end
   end
 
